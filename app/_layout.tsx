250c8317d5c396c5af0ff8b3bc07279d911f7154
VishnuDileesh <munnadileesh@gmail.com>
2025-02-17 20:16:08 +0530

    Setup NativeWind and fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,9 @@
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
 
+import "./global.css"
+
+
 export default function RootLayout() {
   const [loaded] = useFonts({
     Inter: require("../assets/fonts/Inter-Regular.ttf"),
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { Text, View } from "react-native";
 
+
 export default function Index() {
   return (
     <View
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="font-bold text-2xl text-red-500">Welcome to Palpense</Text>
     </View>
   );
 }
